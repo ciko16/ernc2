775657aa65b5dea527fb491d34b4323f59201920
Ciko16 <ciko16@mail.com>
2024-08-15 03:08:23 +0700

    grafik kalender fix

--- assets/js/demo/chart-bar-demo.js
+++ assets/js/demo/chart-bar-demo.js
@@ -91,18 +91,28 @@ function initializeChart(dataPerBulan) {
                 }
             }],
         },
-        plugins: {
-            legend: {
-                display: false
-            },
-            tooltip: {
-                callbacks: {
-                    label: function(tooltipItem) {
-                        return tooltipItem.dataset.label + ': ' + number_format(tooltipItem.raw);
-                    }
+        legend: {
+            display: false
+        },
+        tooltips: {
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+            callbacks: {
+                label: function(tooltipItem, chart) {
+                    var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                    return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
                 }
             }
-        }
+        },
     }
 });
 }
