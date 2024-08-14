dec50575079232102e4914b73259e3aaa68be502
Ciko16 <ciko16@mail.com>
2024-08-15 02:42:45 +0700

    kita ubah nama menjadi chart-bar-demo.js

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -89,7 +89,8 @@
         console.log('dataPerBulan: ', dataPerBulan); // Untuk memastikan variabel terisi dengan benar
     </script>
 
-  
+    <!-- Script untuk inisialisasi chart -->
+    <script src="<?= base_url('assets/js/demo/chart-bar-demo.js')?>"></script>
     <script>
         // Pastikan untuk memanggil fungsi initializeChart setelah file datakalender.js dimuat
         initializeChart(dataPerBulan);
