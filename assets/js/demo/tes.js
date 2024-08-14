5bd709046b6d084639718c385b5dbe75ab2d1f4e
Ciko16 <ciko16@mail.com>
2024-08-15 03:42:06 +0700

    grafik pendapatan revisi tes

--- assets/js/demo/tes.js
+++ assets/js/demo/tes.js
@@ -55,51 +55,13 @@ function initializeChart(pendapatanLayanan, pendapatanPeminjaman) {
         },
         options: {
             scales: {
-                yAxes: [{
+                y: {
+                    beginAtZero: true,
                     ticks: {
-                        stepSize: 1,
-                        beginAtZero: true,
-                        padding: 10,
-                        callback: function(value, index, values) {
-                            return number_format(value);
-                        }
-                    },
-                    gridLines: {
-                        color: "rgb(234, 236, 244)",
-                        zeroLineColor: "rgb(234, 236, 244)",
-                        drawBorder: false,
-                        borderDash: [2],
-                        zeroLineBorderDash: [2]
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Jumlah Data'// label sumbu y
-                    }
-                }],
-            },
-            legend: {
-                display: false
-            },
-            tooltips: {
-                titleMarginBottom: 10,
-                titleFontColor: '#6e707e',
-                titleFontSize: 14,
-                backgroundColor: "rgb(255,255,255)",
-                bodyFontColor: "#858796",
-                borderColor: '#dddfeb',
-                borderWidth: 1,
-                xPadding: 15,
-                yPadding: 15,
-                displayColors: false,
-                caretPadding: 10,
-                callbacks: {
-                    label: function(tooltipItem, chart) {
-                        var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                        return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+                        stepSize: 1
                     }
                 }
-            },
+            }
         }
     });
 }
-
