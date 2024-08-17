55baf17f11964f326aa9ca6b244a147d65174fdc
Ciko16 <ciko16@mail.com>
2024-08-17 23:40:45 +0700

    idle logout auth fix

--- assets/js/idle-logout.js
+++ assets/js/idle-logout.js
@@ -2,7 +2,7 @@ var idleTime = 0;
 
 $(document).ready(function(){
     // Mengatur interval timer menjadi 10 menit (600.000 milidetik)
-    var idleInterval = setInterval(timerIncrement, 600); // 10 menit
+    var idleInterval = setInterval(timerIncrement, 600000); // 10 menit
 
     // Reset timer saat ada aktivitas
     $(this).mousemove(function (e) {
