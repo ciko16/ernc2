7e111ea26dee69cbaf93ec242f868e70cb64851e
Ciko16 <ciko16@mail.com>
2024-08-18 15:09:01 +0700

    update jumlah inventaris ketika di book di tabel kalender 4

--- application/views/tabelkalender/vw_tambah_tb_kalender.php
+++ application/views/tabelkalender/vw_tambah_tb_kalender.php
@@ -20,6 +20,12 @@ Form Tambah Data Kalender
     <label for="booking">Booking</label>
     <select name="booking" id="booking" class="form-control">
         <option value="">Pilih Booking</option>
+        <?php
+        // Debugging output
+        echo '<pre>';
+        print_r($inventaris);
+        echo '</pre>';
+        ?>
         <?php foreach($inventaris as $item): ?>
             <option value="<?= $item['id']; ?>">
                 <?= $item['nama']; ?> - Jumlah: <?= $item['jumlah']; ?>
@@ -27,7 +33,8 @@ Form Tambah Data Kalender
         <?php endforeach; ?>
     </select>
     <?= form_error('booking', '<small class="text-danger pl-3">', '</small>');?>
-    </div>
+</div>
+
     <div class="form-group">
         <label for="isi">Deskripsi</label>
         <input type="text"name="isi" class="form-control" id="isi" placeholder="Deskripsi">
