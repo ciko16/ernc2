6a4f5c3dd015e64058948416cc3882319a9d0c94
Ciko16 <ciko16@mail.com>
2024-08-18 14:55:35 +0700

    update jumlah inventaris ketika di book di tabel kalender3

--- application/views/tabelkalender/vw_tb_kalender.php
+++ application/views/tabelkalender/vw_tb_kalender.php
@@ -38,9 +38,9 @@
         <td><?= $item['booking_name']; ?></td>
         <td><?= $item['isi']; ?></td>
         <td>
-            <!-- <a href="<?= base_url('TabelKalender/detail/'.$kb['id']);?>" class="badge badge-info">Detail</a> -->
-            <a href="<?= base_url('TabelKalender/edit/'.$kb['id']);?>" class="text-warning">Edit</a>
-            <a href="<?= base_url('TabelKalender/hapus/'.$kb['id']);?>" class="text-danger" onclick="javascript: return confirm('Anda yakin untuk menghapus data?')">Hapus</a>
+            <!-- <a href="<?= base_url('TabelKalender/detail/'.$item['id']);?>" class="badge badge-info">Detail</a> -->
+            <a href="<?= base_url('TabelKalender/edit/'.$item['id']);?>" class="text-warning">Edit</a>
+            <a href="<?= base_url('TabelKalender/hapus/'.$item['id']);?>" class="text-danger" onclick="javascript: return confirm('Anda yakin untuk menghapus data?')">Hapus</a>
         </td>
     </tr>
     <?php $i++; ?>
