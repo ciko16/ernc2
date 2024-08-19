f23271a2296fff82c8827471520563bca8226a8a
Ciko16 <ciko16@mail.com>
2024-08-19 15:24:05 +0700

    memberikan pagination pada halaman biaya layanan

--- application/controllers/BiayaLayanan.php
+++ application/controllers/BiayaLayanan.php
@@ -12,26 +12,29 @@ public function __construct()
 public function index()
 {
     $config['base_url'] = site_url('BiayaLayanan/index');
-        $config['total_rows'] = $this->db->count_all('biaya_layanan');
-        $config['per_page'] = 5;
-        $config['uri_segment'] = 3;
-        $choice = $config['total_rows'] / $config['per_page'];
-        $config['num_links'] = floor($choice);
-
-        $config['full_tag_open'] = '<div class="pagging text-center"><nav><ul class="pagination justify-content-center pagination-green">';
-        $config['full_tag_close'] = '</ul></nav></div>';
-        $config['num_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
-        $config['num_tag_close'] = '</span></li>';
-        $config['cur_tag_open'] = '<li class="page-item active pagination-green"><span class="page-link">';
-        $config['cur_tag_close'] = '</span></li>';
-        $config['next_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
-        $config['next_tagl_close'] = '<span aria-hidden="true">&raquo</span></span></li>';
-        $config['prev_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
-        $config['prev_tagl_close'] = '</span>Selanjutnya</li>';
-        $config['first_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
-        $config['first_tagl_close'] = '</span></li>';
-        $config['last_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
-        $config['last_tagl_close'] = '</span></li>';
+    $config['total_rows'] = $this->db->count_all('biaya_layanan');
+    $config['per_page'] = 5;
+    $config['uri_segment'] = 3;
+    $choice = $config['total_rows'] / $config['per_page'];
+    $config['num_links'] = floor($choice);
+    $config['first_link'] = 'First';
+    $config['last_link'] = 'Last';
+    $config['next_link'] = 'Selanjutnya';
+    $config['prev_link'] = 'Sebelumnya';
+    $config['full_tag_open'] = '<div class="pagging text-center"><nav><ul class="pagination justify-content-center pagination-green">';
+    $config['full_tag_close'] = '</ul></nav></div>';
+    $config['num_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
+    $config['num_tag_close'] = '</span></li>';
+    $config['cur_tag_open'] = '<li class="page-item active pagination-green"><span class="page-link">';
+    $config['cur_tag_close'] = '</span></li>';
+    $config['next_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
+    $config['next_tagl_close'] = '<span aria-hidden="true">&raquo</span></span></li>';
+    $config['prev_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
+    $config['prev_tagl_close'] = '</span>&raquo;</li>';
+    $config['first_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
+    $config['first_tagl_close'] = '</span></li>';
+    $config['last_tag_open'] = '<li class="page-item pagination-green"><span class="page-link">';
+    $config['last_tagl_close'] = '</span></li>';
        // Fetch the current page number from the URL and ensure it's valid
        $page = $this->uri->segment($config['uri_segment']);
        if ($page === null) {
