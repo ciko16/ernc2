8ba42e823a2d370b4a04b8972bbdb0479609fbea
Ciko16 <ciko16@mail.com>
2024-08-19 17:02:18 +0700

    memberikan legend pada grafik pendapatan

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -31,7 +31,7 @@
     </style>
     <div class="row">
         <div class="col-lg-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
