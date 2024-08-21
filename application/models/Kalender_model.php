088edbc6bb27c2ee39ca98be00cfb9e6c3ae8411
Ciko16 <ciko16@mail.com>
2024-08-22 01:01:32 +0700

    tes3

--- application/models/Kalender_model.php
+++ application/models/Kalender_model.php
@@ -15,49 +15,49 @@ class Kalender_model extends CI_Model {
         $this->prefs['template'] = '
         {table_open}<table border="0" cellpadding="0" cellspacing="0" class="calender">{/table_open}
 
-{heading_row_start}<tr>{/heading_row_start}
+        {heading_row_start}<tr>{/heading_row_start}
 
-{heading_previous_cell}<th><a href="{previous_url}">&lt;&lt;</a></th>{/heading_previous_cell}
-{heading_title_cell}<th colspan="{colspan}">{heading}</th>{/heading_title_cell}
-{heading_next_cell}<th><a href="{next_url}">&gt;&gt;</a></th>{/heading_next_cell}
+        {heading_previous_cell}<th><a href="{previous_url}">&lt;&lt;</a></th>{/heading_previous_cell}
+        {heading_title_cell}<th colspan="{colspan}">{heading}</th>{/heading_title_cell}
+        {heading_next_cell}<th><a href="{next_url}">&gt;&gt;</a></th>{/heading_next_cell}
 
-{heading_row_end}</tr>{/heading_row_end}
+        {heading_row_end}</tr>{/heading_row_end}
 
-{week_row_start}<tr>{/week_row_start}
-{week_day_cell}<td>{week_day}</td>{/week_day_cell}
-{week_row_end}</tr>{/week_row_end}
+        {week_row_start}<tr>{/week_row_start}
+        {week_day_cell}<td>{week_day}</td>{/week_day_cell}
+        {week_row_end}</tr>{/week_row_end}
 
-{cal_row_start}<tr class="days">{/cal_row_start}
-{cal_cell_start}<td>{/cal_cell_start}
-{cal_cell_start_today}<td>{/cal_cell_start_today}
-{cal_cell_start_other}<td class="other-month">{/cal_cell_start_other}
+        {cal_row_start}<tr class="days">{/cal_row_start}
+        {cal_cell_start}<td>{/cal_cell_start}
+        {cal_cell_start_today}<td>{/cal_cell_start_today}
+        {cal_cell_start_other}<td class="other-month">{/cal_cell_start_other}
 
-{cal_cell_content}
-    <div class="day_num filled">{day}</div>
-    <div class="content">{content}</div>
-{/cal_cell_content}
-{cal_cell_content_today}
-    <div class="day_num highlight filled">{day}</div>
-    <div class="content">{content}</div>
-{/cal_cell_content_today}
+         {cal_cell_content}
+            <div class="day_num filled">{day}</div>
+            <div class="content">{content}</div>
+        {/cal_cell_content}
+        {cal_cell_content_today}
+        <div class="day_num highlight filled">{day}</div>
+        <div class="content">{content}</div>
+        {/cal_cell_content_today}
 
-{cal_cell_no_content}
-    <div class="day_num empty">{day}</div>
-{/cal_cell_no_content}
-{cal_cell_no_content_today}
-    <div class="day_num highlight empty">{day}</div>
-{/cal_cell_no_content_today}
+        {cal_cell_no_content}
+            <div class="day_num empty">{day}</div>
+        {/cal_cell_no_content}
+        {cal_cell_no_content_today}
+            <div class="day_num highlight empty">{day}</div>
+        {/cal_cell_no_content_today}
 
-{cal_cell_blank}&nbsp;{/cal_cell_blank}
+        {cal_cell_blank}&nbsp;{/cal_cell_blank}
 
-{cal_cell_other}{day}{/cal_cell_other}
+        {cal_cell_other}{day}{/cal_cell_other}
 
-{cal_cell_end}</td>{/cal_cell_end}
-{cal_cell_end_today}</td>{/cal_cell_end_today}
-{cal_cell_end_other}</td>{/cal_cell_end_other}
-{cal_row_end}</tr>{/cal_row_end}
+        {cal_cell_end}</td>{/cal_cell_end}
+        {cal_cell_end_today}</td>{/cal_cell_end_today}
+        {cal_cell_end_other}</td>{/cal_cell_end_other}
+        {cal_row_end}</tr>{/cal_row_end}
 
-{table_close}</table>{/table_close}
+        {table_close}</table>{/table_close}
         ';
     }
 
@@ -86,9 +86,9 @@ class Kalender_model extends CI_Model {
         $calendar_date = date("j", strtotime($row->tanggal)); // Ambil tanggal hari saja
         $day_url = base_url("kalender/detail/" . $row->tanggal); // buat URL ke halaman detail
         // Gabungkan tanggal dengan HTML yang mengandung link
-        $cal_data[(int)$calendar_date] = '<a href="' . $day_url . '">Detail</a>';
-                                        // . '<div class="day_num">Detail</div>'
-                                        // . '</a>';
+        $cal_data[(int)$calendar_date] = '<a href="' . $day_url . '">'
+                                        . '<div class="day_num">Detail</div>'
+                                        . '</a>';
                                         // . '<div class="calendar-isi">' . $row->isi . '</div>'
                                         // . '<div class="calendar-booking">' . $row->booking . '</div>';
     }
